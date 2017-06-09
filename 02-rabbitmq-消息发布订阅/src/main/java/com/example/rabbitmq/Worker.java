bd7b69a30e2e99fbf52278d68e1f1c17e58fec99
王俊超 <shining-glory@qq.com>
2017-06-09 08:12:30 +0800

    commit

---
+++ 02-rabbitmq-消息发布订阅/src/main/java/com/example/rabbitmq/Worker.java
@@ -0,0 +1,58 @@
+package com.example.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-06-09 08:02
+ * All Rights Reserved !!!
+ */
+public class Worker {
+    public static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // 指定从消息通道中每次取的消息数量
+        channel.basicQos(1);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                    AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } finally {
+                    System.out.println(" [x] Done");
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            }
+        };
+
+        channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
