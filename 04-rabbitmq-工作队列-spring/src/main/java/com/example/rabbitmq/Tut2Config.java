f4e04b0691e164a9b6ed3c38601c83c80db08de3
王俊超 <shining-glory@qq.com>
2017-06-17 11:02:06 +0800

    commit

---
+++ 04-rabbitmq-工作队列-spring/src/main/java/com/example/rabbitmq/Tut2Config.java
@@ -0,0 +1,53 @@
+package com.example.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * RabbitMQ消息配置
+ * Author: 王俊超
+ * Date: 2017-06-17 10:26
+ * All Rights Reserved !!!
+ */
+@Profile({"tut2", "work-queues"})
+@Configuration
+public class Tut2Config {
+    /**
+     * 创建一个消息队列
+     * @return
+     */
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    /**
+     * 定一个接收配置对象，定义两个接收者
+     */
+    @Profile("receiver")
+    private static class ReceiverConfig {
+
+        @Bean
+        public Tut2Receiver receiver1() {
+            return new Tut2Receiver(1);
+        }
+
+        @Bean
+        public Tut2Receiver receiver2() {
+            return new Tut2Receiver(2);
+        }
+    }
+
+    /**
+     * 定义一个消息发送对象
+     *
+     * @return
+     */
+    @Profile("sender")
+    @Bean
+    public Tut2Sender sender() {
+        return  new Tut2Sender();
+    }
+}
---
+++ 04-rabbitmq-工作队列-spring/src/main/java/com/example/rabbitmq/Tut2Receiver.java
@@ -0,0 +1,46 @@
+package com.example.rabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.util.StopWatch;
+
+/**
+ * 消息接收者对象，并指定接收的消息队列
+ * <p>
+ * Author: 王俊超
+ * Date: 2017-06-17 10:30
+ * All Rights Reserved !!!
+ */
+@RabbitListener(queues = "hello")
+public class Tut2Receiver {
+
+    private final int instance;
+
+
+    public Tut2Receiver(int i) {
+        this.instance = i;
+    }
+
+    /**
+     * 指定消息接收后的处理方法，因为发送的消息是string类型，所以接收方法的入参也是string类型
+     *
+     * @param in
+     * @throws InterruptedException
+     */
+    @RabbitHandler
+    public void receive(String in) throws InterruptedException {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        System.out.println("instance " + this.instance + " [x] Received '" + in + "'");
+        doWork(in);
+        System.out.println("instance " + this.instance + " [x] Done in " + watch.getTotalTimeSeconds() + "s");
+    }
+
+    private void doWork(String in) throws InterruptedException {
+        for (char ch : in.toCharArray()) {
+            if (ch == '.') {
+                Thread.sleep(1000);
+            }
+        }
+    }
+}
