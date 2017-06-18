903194e38db7bb1d375c57f7a590e14bdb9da9b6
王俊超 <shining-glory@qq.com>
2017-06-18 20:53:43 +0800

    commit

--- 02-rabbitmq-hello-world-spring/src/main/java/com/example/rabbitmq/spring/RabbitAmqpTutorialsApplication.java
+++ 02-rabbitmq-hello-world-spring/src/main/java/com/example/rabbitmq/spring/RabbitAmqpTutorialsApplication.java
@@ -1,10 +1,7 @@
 package com.example.rabbitmq.spring;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -17,12 +14,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class RabbitAmqpTutorialsApplication {
-
-    @Bean
-    public CommandLineRunner tutorial() {
-        return new RabbitAmqpTutorialsRunner();
-    }
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(RabbitAmqpTutorialsApplication.class, args);
     }
